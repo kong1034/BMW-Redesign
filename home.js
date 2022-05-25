9041549340b44787bf4b4762422b0675684ef54d
MASTER <>
2022-05-25 23:08:22 +0900

    Kong - Nav Menu Complete. - Rayout Complete. - Hover Event by Menu Complete. - Search Function Complete.

---
+++ home.js
@@ -0,0 +1,37 @@
+let header_ul = document.querySelector('.header_ul');
+let header_li = document.querySelectorAll('.header_li');
+let header_li_a = document.querySelectorAll('.header_li_a');
+let search_box = document.querySelector('.search_box');
+let search_icon = document.querySelector('.search_icon');
+let search_input = document.querySelector('.search_input');
+let cancel_icon = document.querySelector('.cancel_icon');
+
+header_li_a.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.style.borderBottom = '1px solid #0653b6';
+        element.style.color = '#0653b6';
+    })
+    element.addEventListener('mouseout', () => {
+        element.style.borderBottom = '';
+        element.style.color = '#ffffff';
+    })
+});
+
+search_icon.addEventListener('click', () => {
+    header_li.forEach(element => {
+        element.style.display = 'none';
+    })
+    search_icon.style.display = 'none';
+    cancel_icon.style.display = 'block';
+    search_box.style.display = 'block';  
+})
+
+cancel_icon.addEventListener('click', () => {
+    header_li.forEach(element => {
+        element.style.display = 'block';
+    })
+    search_icon.style.display = 'block';
+    cancel_icon.style.display = 'none';
+    search_box.style.display = 'none';
+    search_input.value = '';
+})
