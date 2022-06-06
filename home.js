f867985ceaff5ef1c730525abe45bdde4b1aec16
KONG1034 <kyh93054@naver.com>
2022-06-06 23:51:57 +0900

    Kong - desk podcast design clear & fnc developing

--- home.js
+++ home.js
@@ -15,6 +15,15 @@ let m_header_overlay = document.querySelector('.m_header_overlay');
 let m_header_cancel_img = document.querySelector('.m_header_cancel_img');
 let main_bg_box = document.querySelector('.main_bg_box');
 let main_bg_idx = 0;
+let pod_left_arrow_box = document.querySelector('.pod_left_arrow_box');
+let pod_right_arrow_box = document.querySelector('.pod_right_arrow_box');
+let pod_left_arrow = document.querySelector('.pod_left_arrow');
+let pod_right_arrow = document.querySelector('.pod_right_arrow');
+let podcast_img_container = document.querySelector('.podcast_img_container');
+let bmw_podcast_container = document.querySelector('.bmw_podcast_container');
+let pod_img_idx = 0;
+
+/* nav 메뉴 */
 header_li_a.forEach(element => {
     element.addEventListener('mouseover', () => {
         element.style.borderBottom = '1px solid #0653b6';
@@ -25,7 +34,7 @@ header_li_a.forEach(element => {
         element.style.color = '#ffffff';
     })
 });
-
+/* 검색 */
 search_icon.addEventListener('click', () => {
     header_li.forEach(element => {
         element.style.display = 'none';
@@ -34,7 +43,6 @@ search_icon.addEventListener('click', () => {
     cancel_icon.style.display = 'block';
     search_box.style.display = 'block';  
 })
-
 cancel_icon.addEventListener('click', () => {
     header_li.forEach(element => {
         element.style.display = 'block';
@@ -44,7 +52,7 @@ cancel_icon.addEventListener('click', () => {
     search_box.style.display = 'none';
     search_input.value = '';
 })
-
+/* 모바일 nav 메뉴 */
 tab_menu_a.addEventListener('click', () => {
     m_header_overlay.classList.add('visible');
     right_arrow_box.style.display = 'none';
@@ -55,6 +63,7 @@ m_header_cancel_img.addEventListener('click', () => {
     right_arrow_box.style.display = 'block';
     left_arrow_box.style.display = 'block';
 })
+/* 메인 이미지 슬라이더 */
 right_arrow_box.addEventListener('click', () => {
     main_bg_idx -= 100;
     console.log('check main_bg_idx => '+main_bg_idx);
@@ -79,5 +88,31 @@ left_arrow_box.addEventListener('click', () => {
     if(main_bg_idx == 0){
         left_arrow_box.style.display = 'none';
         left_arrow.style.display = 'none';
+    }
+})
+/* podcast 이미지 슬라이더 */
+pod_left_arrow_box.addEventListener('click', () => {
+    pod_img_idx += 70;
+    podcast_img_container.style.transform = `translateX(${pod_img_idx}vw)`;
+    console.log('left pod img idx check '+pod_img_idx);
+    if(pod_img_idx < 0) {
+        pod_right_arrow_box.style.display = 'block';
+        pod_right_arrow.style.display = 'block';
+    }
+    if(pod_img_idx == 0){
+        pod_left_arrow_box.style.display = 'none';
+        pod_left_arrow.style.display = 'none';
+    }
+});
+pod_right_arrow_box.addEventListener('click', () => {
+    pod_img_idx -= 70;
+    podcast_img_container.style.transform = `translateX(${pod_img_idx}vw)`;
+    console.log('right pod img idx check '+pod_img_idx);
+    if(pod_img_idx < 0) {
+        pod_left_arrow_box.style.display = 'block';
+        pod_left_arrow.style.display = 'block';
+    }
+    if(pod_img_idx == -140){
+        pod_right_arrow_box.style.display = 'none';
     }
 })
